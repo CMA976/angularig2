e28d4487a9fdacac4e945c8bde28ab98afbfe35b
Pedro Hurtado <pedrohurt@gmail.com>
2024-11-07 11:47:44 +0100

    if

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  name = 'Pedro'
+  isAutenticated = false
 }
