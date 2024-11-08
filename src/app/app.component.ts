0d2b87b76f23f6f24bf1aa9807f25b35868fe31c
Pedro Hurtado <pedrohurt@gmail.com>
2024-11-08 12:32:13 +0100

    custom events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GridComponent } from './calendar/grid/grid.component';
 import { CalendarComponent } from './calendar/calendar/calendar.component';
+import { ListpizzasComponent } from './pizzas/listpizzas/listpizzas.component';
+import { CarritoComponent } from './pizzas/carrito/carrito.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CalendarComponent],
+  imports: [RouterOutlet, CalendarComponent, ListpizzasComponent, CarritoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/pizzas/pizzaitem/pizzaitem.component.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { Pizza } from '../pizza';
+
+@Component({
+  selector: 'app-pizzaitem',
+  standalone: true,
+  imports: [],
+  templateUrl: './pizzaitem.component.html',
+  styleUrl: './pizzaitem.component.css'
+})
+export class PizzaitemComponent {
+  pizza = input.required<Pizza>()
+}
